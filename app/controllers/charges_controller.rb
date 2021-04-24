2c1b38bc996428e6ad68a833b5783562c3a48306
sazanami16 <za_nami.za_nami16@icloud.com>
2021-04-24 23:33:10 +0900

    new,createアクションの記述

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,4 +1,19 @@
 class ChargesController < ApplicationController
   def index
   end
+
+  def new
+    @charge = Charge.new
+  end
+
+  def create
+    @charge = Charge.new(charge_params)
+  end
+
+
+  private
+  def charge_params
+    params.require(:charge).permit(:tax_name, :money, :deadline, :place, :address, :number, :payment)
+  end
+  
 end
