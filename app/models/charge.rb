4d0f6597b913a617a342ec4a8c1bf79a8522c7ea
sazanami16 <za_nami.za_nami16@icloud.com>
2021-04-24 23:32:44 +0900

    モデルの追加

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,2 +1,9 @@
 class Charge < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :tax_name
+    validates :money
+    validates :deadline
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :charges
+
+  with_options presence: true do
+    validates :name
+    validates :email
+    validates :password
+  end
 end
