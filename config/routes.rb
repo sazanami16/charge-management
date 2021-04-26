c7a710e446a53d00aa8220f7b7362f2553ad3af9
sazanami16 <za_nami.za_nami16@icloud.com>
2021-04-26 23:56:11 +0900

    ユーザーログインと新規登録の機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root to: "charges#index"
+  
+  get 'login' => 'sessions#new'
+  post 'login'=> 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
+  get 'signup' => 'users#new'
+
+  resources :users, only: [:create]
   resources :charges
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_04_17_140911) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.string "password", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
